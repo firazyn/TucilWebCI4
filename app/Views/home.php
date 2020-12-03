2ed643ff9bf4dd4a819e9642b7433ce98b94550a
firazyn <fyn99.178@gmail.com>
2020-12-03 22:06:06 +0700

    simplified views

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,27 +1,3 @@
-  <section>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="https://www.youtube.com/watch?v=YddwkMJG1Jo">Firaz #MajuDPM</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= site_url('') ?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= site_url('admin') ?>">Admin</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= site_url('about') ?>">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= site_url('biodata') ?>">Biodata</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 <div class="text-container">
 			<p>Dukung Firaz</p>
 		    <p>#MajuDPM</p>
